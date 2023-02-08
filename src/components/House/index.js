b00ccf49a65101617777e0e603f6be6ef545af09
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-02-08 16:29:02 -0300

    fix: create condicional renderind

--- src/components/House/index.js
+++ src/components/House/index.js
@@ -3,20 +3,22 @@ import "./House.css";
 
 export const House = (props) => {
   return (
-    <section
-      className="house-container"
-      style={{ backgroundColor: props.secondary }}
-    >
-      <h3 style={{ borderColor: props.primary }}>{props.name}</h3>
-      <div className="character-container">
-        {props.characters.map((character) => (
-          <Character
-            image={character.imagem}
-            name={character.nome}
-            headOffice={character.sede}
-          />
-        ))}
-      </div>
-    </section>
+    props.characters.length > 0 && (
+      <section
+        className="house-container"
+        style={{ backgroundColor: props.secondary }}
+      >
+        <h3 style={{ borderColor: props.primary }}>{props.name}</h3>
+        <div className="character-container">
+          {props.characters.map((character) => (
+            <Character
+              image={character.imagem}
+              name={character.nome}
+              headOffice={character.sede}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
